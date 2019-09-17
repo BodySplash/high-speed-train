14e52ef21c567581d02de8ec67a81dc49d46ba9f
Jean-Baptiste DUSSEAUT <jbdusseaut@arpinum.fr>
2019-09-17 23:04:18 +0200

    fix: archive shared channel configuration hell chore: brings some colors to logging

--- reservation/common/src/main/java/common/aeron/cluster/Contexts.java
+++ reservation/common/src/main/java/common/aeron/cluster/Contexts.java
@@ -1,6 +1,8 @@
 package common.aeron.cluster;
 
+import io.aeron.ChannelUriStringBuilder;
 import io.aeron.archive.*;
+import io.aeron.archive.client.AeronArchive;
 import io.aeron.cluster.ConsensusModule;
 import io.aeron.cluster.service.ClusteredServiceContainer;
 import io.aeron.driver.*;
@@ -9,26 +11,51 @@ import org.agrona.concurrent.*;
 import java.io.File;
 import java.util.function.Supplier;
 
+import static io.aeron.CommonContext.*;
+
 public class Contexts {
 
     public static Contexts build(ClusterNode.Configuration configuration) {
         String aeronDirectoryName = String.join(File.separator, configuration.rootDirectory(), "driver");
         String archiveDirectoryName = String.join(File.separator, configuration.rootDirectory(), "archive");
         String clusterDirectoryName = String.join(File.separator, configuration.rootDirectory(), "cluster");
+
+        var archiveControlChannel = new ChannelUriStringBuilder()
+                .media(UDP_MEDIA)
+                .endpoint(String.join(":", "localhost", "8010"))
+                .build();
+
+        var archiveResponseChannel = new ChannelUriStringBuilder()
+                .media(UDP_MEDIA)
+                .endpoint(String.join(":", "localhost", "8020"))
+                .build();
+
+        var archiveEventsChannel = new ChannelUriStringBuilder()
+                .media(UDP_MEDIA)
+                .controlEndpoint(String.join(":", "localhost", "8030"))
+                .controlMode(MDC_CONTROL_MODE_DYNAMIC)
+                .build();
+
         var idleStrategySupplier = supplierFor(configuration);
         var driverCtx = new MediaDriver.Context()
                 .aeronDirectoryName(aeronDirectoryName)
                 .threadingMode(driverThreadingMode(configuration))
                 .conductorIdleStrategy(idleStrategySupplier.get())
                 .receiverIdleStrategy(idleStrategySupplier.get())
+                .spiesSimulateConnection(true)
                 .senderIdleStrategy(idleStrategySupplier.get());
         var archiveCtx = new Archive.Context()
                 .aeronDirectoryName(aeronDirectoryName)
                 .archiveDirectoryName(archiveDirectoryName)
+                .controlChannel(archiveControlChannel)
+                .recordingEventsChannel(archiveEventsChannel)
                 .idleStrategySupplier(idleStrategySupplier)
                 .threadingMode(archiveThreadingMode(configuration));
         var consensusModuleCtx = new ConsensusModule.Context().aeronDirectoryName(aeronDirectoryName)
                 .appointedLeaderId(0)
+                .archiveContext(new AeronArchive.Context()
+                        .controlRequestChannel(archiveControlChannel)
+                        .controlResponseChannel(archiveResponseChannel))
                 .aeronDirectoryName(aeronDirectoryName)
                 .clusterDirectoryName(clusterDirectoryName)
                 .idleStrategySupplier(idleStrategySupplier)
@@ -43,7 +70,6 @@ public class Contexts {
 
     private static ArchiveThreadingMode archiveThreadingMode(ClusterNode.Configuration configuration) {
         switch (configuration.profile()) {
-
             case PERF:
                 return ArchiveThreadingMode.DEDICATED;
             case SLOW:
