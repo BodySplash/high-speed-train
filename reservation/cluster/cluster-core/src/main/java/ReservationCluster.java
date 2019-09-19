baaba77e30fabb5cde2821836dbd3f7ca65362b2
Jean-Baptiste DUSSEAUT <jbdusseaut@arpinum.fr>
2019-09-19 22:16:33 +0200

    cleanup for a fresh start

--- reservation/cluster/cluster-core/src/main/java/ReservationCluster.java
+++ reservation/cluster/cluster-core/src/main/java/ReservationCluster.java
@@ -1,15 +1,6 @@
-import common.aeron.node.*;
-import reservation.cluster.ReservationService;
-
 public class ReservationCluster {
 
     public static void main(String[] args) {
-        try (var launch = ClusterNode.launch(ClusterNode.configuration()
-                .profile(Profile.SLOW)
-                .memberId(0)
-                .rootDirectory("./data")
-                .service(new ReservationService()))) {
-            launch.awaitShutDown();
-        }
+        // todo
     }
 }
